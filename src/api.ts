0bfc0f4f341ed804e27216aa82e90e61756c4952
Kishan Bagaria <KishanBagaria@users.noreply.github.com>
2020-12-08 00:49:05 +0530

    sendTypingIndicator → sendActivityIndicator

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import matchSorter from 'match-sorter'
 import os from 'os'
 import { promises as fs } from 'fs'
 import { WAConnection, WA_MESSAGE_STATUS_TYPE, STORIES_JID, MessageType, MessageOptions, Mimetype, Presence, Browsers, ChatModification, WATextMessage, BaileysError, isGroupID, whatsappID, ReconnectMode, UNAUTHORIZED_CODES, promiseTimeout, WAChat, WAChatUpdate, WA_MESSAGE_ID, WAGroupMetadata, WAContact, WAMessageProto } from '@adiwajshing/baileys'
-import { texts, PlatformAPI, OnServerEventCallback, MessageSendOptions, InboxName, LoginResult, ConnectionState, ConnectionStatus, ServerEventType, OnConnStateChangeCallback, ReAuthError, CurrentUser, ServerEvent, MessageContent, ConnectionError, PaginationArg, AccountInfo, Message } from '@textshq/platform-sdk'
+import { texts, PlatformAPI, OnServerEventCallback, MessageSendOptions, InboxName, LoginResult, ConnectionState, ConnectionStatus, ServerEventType, OnConnStateChangeCallback, ReAuthError, CurrentUser, ServerEvent, MessageContent, ConnectionError, PaginationArg, AccountInfo, ActivityType } from '@textshq/platform-sdk'
 
 import { mapMessage, mapMessages, mapContact, mapThreads, mapThread, mapThreadProps, mapPresenceUpdate } from './mappers'
 import { hasUrl, isBroadcastID, numberFromJid, textsWAKey } from './util'
@@ -461,11 +461,14 @@ export default class WhatsAppAPI implements PlatformAPI {
     await this.client.chatRead(threadID, 'read')
   }
 
-  sendTypingIndicator = async (threadID: string, typing: boolean) => {
-    texts.log('send typing:', typing, 'to', threadID, typing)
-    if (this.client.state === 'open') {
-      await this.client.updatePresence(threadID, typing ? Presence.composing : Presence.paused)
-    }
+  sendActivityIndicator = async (type: ActivityType, threadID: string) => {
+    if (this.client.state !== 'open') return
+    const presence = {
+      [ActivityType.NONE]: Presence.paused,
+      [ActivityType.TYPING]: Presence.composing,
+      [ActivityType.RECORDING_VOICE]: Presence.recording,
+    }[type]
+    await this.client.updatePresence(threadID, presence)
   }
 
   changeThreadTitle = async (threadID: string, newTitle: string) => {
