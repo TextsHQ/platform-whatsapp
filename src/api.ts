94939772993840611c17ce0c376cb946d4c06164
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-07-11 23:23:21 +0530

    update client version

--- src/api.ts
+++ src/api.ts
@@ -46,7 +46,7 @@ export default class WhatsAppAPI implements PlatformAPI {
   private lastConnStatus: ConnectionStatus = null
 
   constructor() {
-    this.client.version = [2, 2123, 7]
+    this.client.version = [2, 2126, 10]
     this.client.logger.level = texts.IS_DEV ? 'debug' : 'silent'
     this.client.browserDescription = Browsers.appropriate('Chrome')
     this.client.autoReconnect = ReconnectMode.onConnectionLost
