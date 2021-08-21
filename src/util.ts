a7464034d119604624776ede4c6c2299415e9ca7
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-08-21 18:06:11 +0530

    add comments

--- src/util.ts
+++ src/util.ts
@@ -60,10 +60,14 @@ export const makeMutex = () => {
   return {
     mutex<T>(code: () => Promise<T>):Promise<T> {
       task = (async () => {
+        // wait for the previous task to complete
+        // if there is an error, we swallow so as to not block the queue
         try { await task } catch { }
-        const result = await code()
-        return result
+        // execute the current task
+        return code()
       })()
+      // we replace the existing task, appending the new piece of execution to it
+      // so the next task will have to wait for this one to finish
       return task
     },
   }
