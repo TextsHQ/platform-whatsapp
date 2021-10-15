40809d6188eac2b5d0eb535bc44c3ab4a0e4f444
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-10-16 00:56:20 +0530

    bump version, change name, fix for loading bs3 externally

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ export default {
   },
 
   get api() {
+    const nmPath = require('path').join(texts.constants.BUILD_DIR_PATH, '../node_modules')
+    require('module').globalPaths.push(nmPath) // fix for bs3
+
     return require('./api').default
   },
 } as Platform
