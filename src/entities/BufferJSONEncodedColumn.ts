813ae6d5938cd327de7fd0db37ad246fa01e73c0
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-12-09 22:26:05 +0530

    fix: do not use blob for BufferJSON column

--- src/entities/BufferJSONEncodedColumn.ts
+++ src/entities/BufferJSONEncodedColumn.ts
@@ -2,7 +2,7 @@ import type { ColumnOptions } from 'typeorm'
 import { BufferJSON } from '@adiwajshing/baileys-md'
 
 const BufferJSONEncodedColumn: ColumnOptions = {
-  type: 'blob',
+  type: 'text',
   transformer: {
     from: (buff: string | null) => (buff ? JSON.parse(buff, BufferJSON.reviver) : buff),
     to: (item: any | null) => (item ? JSON.stringify(item, BufferJSON.replacer) : null),
