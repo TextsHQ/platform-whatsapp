e67971c3156b41d0e7a99daeaec728d42ba387bf
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-12-10 19:45:14 +0530

    fix: bufferjson column parsing issue

--- src/entities/BufferJSONEncodedColumn.ts
+++ src/entities/BufferJSONEncodedColumn.ts
@@ -4,7 +4,7 @@ import { BufferJSON } from '@adiwajshing/baileys-md'
 const BufferJSONEncodedColumn: ColumnOptions = {
   type: 'text',
   transformer: {
-    from: (buff: string | null) => (buff ? JSON.parse(buff, BufferJSON.reviver) : buff),
+    from: (buff: string | null) => (typeof buff === 'string' ? JSON.parse(buff, BufferJSON.reviver) : buff),
     to: (item: any | null) => (item ? JSON.stringify(item, BufferJSON.replacer) : null),
   },
 }
