2e0a882b142ff1c9b13b25f2ed5014956f810c67
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-01-21 15:36:28 +0530

    feat: load message info on legacy

--- src/utils/fetch-messages.ts
+++ src/utils/fetch-messages.ts
@@ -1,4 +1,4 @@
-import type { AnyWASocket, WAMessageKey } from '@adiwajshing/baileys'
+import { AnyWASocket, isJidGroup, WAMessageKey } from '@adiwajshing/baileys'
 import type { PaginationArg } from '@textshq/platform-sdk'
 import type { Connection, EntityManager } from 'typeorm'
 import DBMessage from '../entities/DBMessage'
@@ -25,25 +25,48 @@ export default async (
   }
   const items = (await qb.getMany()).reverse()
 
-  if (conn.type === 'legacy' && items.length < MESSAGE_PAGE_SIZE) {
-    const cursorToFetch = items[0]?.cursor || pagination?.cursor
-    let beforeKey: WAMessageKey | undefined
-    if (cursorToFetch) {
-      const [, id] = cursorToFetch.split(',')
-      beforeKey = { ...unmapMessageID(id), remoteJid: threadID }
-    }
-    const msgs = await conn.fetchMessagesFromWA(threadID, WA_MSG_FETCH_SIZE, beforeKey ? { before: beforeKey } : undefined)
-    const dbMessages = msgs.map(m => {
-      const mappedMsg = new DBMessage()
-      mappedMsg.original = { message: m }
-      mappedMsg.mapFromOriginal(mappingCtx)
-      mappedMsg.shouldFireEvent = false
-      return mappedMsg
-    })
-    await db.getRepository(DBMessage).save(dbMessages, { transaction: false })
+  if (conn.type === 'legacy') {
+    if (items.length < MESSAGE_PAGE_SIZE) {
+      const cursorToFetch = items[0]?.cursor || pagination?.cursor
+      let beforeKey: WAMessageKey | undefined
+      if (cursorToFetch) {
+        const [, id] = cursorToFetch.split(',')
+        beforeKey = { ...unmapMessageID(id), remoteJid: threadID }
+      }
+      const msgs = await conn.fetchMessagesFromWA(threadID, WA_MSG_FETCH_SIZE, beforeKey ? { before: beforeKey } : undefined)
+      const dbMessages = msgs.map(m => {
+        const mappedMsg = new DBMessage()
+        mappedMsg.original = { message: m }
+        mappedMsg.mapFromOriginal(mappingCtx)
+        mappedMsg.shouldFireEvent = false
+        return mappedMsg
+      })
+      await db.getRepository(DBMessage).save(dbMessages, { transaction: false })
 
-    const missingMessageCount = MESSAGE_PAGE_SIZE - items.length
-    items.splice(0, 0, ...dbMessages.slice(-missingMessageCount))
+      const missingMessageCount = MESSAGE_PAGE_SIZE - items.length
+      items.splice(0, 0, ...dbMessages.slice(-missingMessageCount))
+    }
+
+    if (isJidGroup(threadID)) {
+      const messagesToSave: DBMessage[] = []
+      await Promise.all(
+        items.map(
+          async message => {
+            if (message.original.message.key.fromMe && !message.original.info) {
+              try {
+                message.original.info = await conn.messageInfo(threadID, message.original.message.key.id!)
+                message.mapFromOriginal(mappingCtx)
+                messagesToSave.push(message)
+              } catch {
+
+              }
+            }
+          },
+        ),
+      )
+
+      await db.getRepository(DBMessage).save(messagesToSave, { transaction: false })
+    }
   }
 
   return {
