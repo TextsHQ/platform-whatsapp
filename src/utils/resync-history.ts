ce36d54be86109470506c900853925c2cc947c3b
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-03-01 17:09:57 +0530

    feat: add dev resync history util

---
+++ src/utils/resync-history.ts
@@ -0,0 +1,26 @@
+import type { WASocket } from '@adiwajshing/baileys'
+import { texts } from '@textshq/platform-sdk'
+import type { Connection, EntityManager } from 'typeorm'
+import DBMessage from '../entities/DBMessage'
+
+/// runs history sync using the stored history messages in the DB
+/// only for dev usage
+const resyncHistory = async (db: Connection | EntityManager, client: WASocket) => {
+  const repo = db.getRepository(DBMessage)
+  const msgs = await repo.find({
+    where: {
+      isHistoryMessage: true,
+    },
+    order: {
+      timestamp: 'ASC',
+    },
+  })
+
+  texts.log(`using ${msgs.length} history messages`)
+
+  for (const msg of msgs) {
+    await client.processMessage(msg.original.message, {})
+  }
+}
+
+export default resyncHistory
