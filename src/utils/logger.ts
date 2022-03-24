ba4d8e5dfc3d8129690e02ab71c65e5118148e33
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-03-24 20:40:57 +0530

    chore: update baileys

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
+import { DEFAULT_CONNECTION_CONFIG } from '@adiwajshing/baileys'
 import { texts } from '@textshq/platform-sdk'
-import P from 'pino'
 
-const logger = P()
+const { logger } = DEFAULT_CONNECTION_CONFIG
 logger.level = texts.isLoggingEnabled ? 'debug' : 'silent'
 
 export default logger
