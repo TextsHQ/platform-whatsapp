e56ba5e862ab6ec2cf5a235e5ee514c4b3b76a95
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-09-05 14:42:29 +0530

    Revert "feat: hide hidden messages on expiry"
    This reverts commit 620043fd379a1c961fca05df001aaedec2e92882.

--- src/entities/DBMessage.ts
+++ src/entities/DBMessage.ts
@@ -150,13 +150,6 @@ export default class DBMessage implements Message {
       item.sortKey = item.orderKey?.toString(10).padStart(10, '0')
     }
 
-    if (!item.isHidden && item.expiresInSeconds && item.timestamp) {
-      const secondsSinceMsg = (Date.now() - item.timestamp.getTime())/1000
-      if (secondsSinceMsg > item.expiresInSeconds) {
-        item.isHidden = true
-      }
-    }
-
     delete item.original
     delete item._original
     delete item.isHistoryMessage
