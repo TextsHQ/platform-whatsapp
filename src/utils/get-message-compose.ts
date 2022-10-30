8754671d57835be1abbdbdb2709b9420d8e9834f
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-10-30 21:01:55 +0530

    don't send image/heic files as image

--- src/utils/get-message-compose.ts
+++ src/utils/get-message-compose.ts
@@ -50,7 +50,7 @@ const getMessageCompose = async (
       let media: AnyMediaMessageContent
       if (mimeType?.endsWith('/webp')) media = { sticker: buffer, ...(msgContent.size || {}) }
       else if (mimeType?.includes('video/mp4')) media = { video: buffer, caption: text, gifPlayback: msgContent.isGif, ...(msgContent.size || {}) }
-      else if (mimeType?.includes('image/') && mimeType !== 'image/gif') media = { image: buffer, caption: text, ...(msgContent.size || {}) }
+      else if (mimeType?.includes('image/') && mimeType !== 'image/gif' && mimeType !== 'image/heic') media = { image: buffer, caption: text, ...(msgContent.size || {}) }
       else if (mimeType?.includes('audio/')) media = { audio: buffer, ptt: mimeType === 'audio/ogg', seconds: msgContent.audioDurationSeconds }
       else media = { document: buffer, fileName: msgContent.fileName, mimetype: '' }
 
