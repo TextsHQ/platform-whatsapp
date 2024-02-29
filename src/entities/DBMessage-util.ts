b3b2182de81aa79478325939c331777a942c93c6
Javier Cuevas <javiercr@gmail.com>
2024-02-29 18:29:02 +0100

    Fix group notifications (set proper sender)

--- src/entities/DBMessage-util.ts
+++ src/entities/DBMessage-util.ts
@@ -91,11 +91,11 @@ export const PRE_DEFINED_MESSAGES: { [k: number]: string | ((m: WAMessage) => st
   [WAMessageStubType.CHANGE_EPHEMERAL_SETTING]: message => getEphemeralMessageSettingChangedText(+message.messageStubParameters![0], message.messageStubParameters![1]),
   [WAMessageStubType.DISAPPEARING_MODE]: message => getDisappearingModeMessageText(+message.messageStubParameters![0], message.messageStubParameters![1]),
 
-  [WAMessageStubType.GROUP_CHANGE_DESCRIPTION]: '{{sender}} changed the group description',
-  [WAMessageStubType.GROUP_PARTICIPANT_REMOVE]: '{{sender}} removed {{{{0}}}} from this group',
-  [WAMessageStubType.GROUP_CHANGE_SUBJECT]: '{{sender}} changed the group subject to "{{0}}"',
-  [WAMessageStubType.GROUP_CHANGE_ICON]: '{{sender}} changed this group\'s icon',
-  [WAMessageStubType.GROUP_PARTICIPANT_INVITE]: '{{sender}} joined using this group\'s invite link',
+  [WAMessageStubType.GROUP_CHANGE_DESCRIPTION]: message => `{{${message.participant}}} changed the group description`,
+  [WAMessageStubType.GROUP_PARTICIPANT_REMOVE]: message => `{{${message.participant}}} removed {{{{0}}}} from this group`,
+  [WAMessageStubType.GROUP_CHANGE_SUBJECT]: message => `{{${message.participant}}} changed the group subject to "{{0}}"`,
+  [WAMessageStubType.GROUP_CHANGE_ICON]: message => `{{${message.participant}}} changed this group's icon'`,
+  [WAMessageStubType.GROUP_PARTICIPANT_INVITE]: message => `{{${message.participant}}} joined using this group's invite link`,
 
   [WAMessageStubType.GROUP_PARTICIPANT_LEAVE]: message =>
     `${message.messageStubParameters!.map(p => `{{${p}}}`).join(', ')} left`,
