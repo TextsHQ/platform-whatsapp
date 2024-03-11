b641758e54040d07dbd058e6acbba90625a57df8
Nurguly Ashyrov <nurguly.ashyrov@automattic.com>
2024-03-11 17:50:20 -0400

    fix(link-preview): set proper user-agent header for link-preview requests

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,8 @@ export const CHAT_MUTE_DURATION_S = 64092211200
 export const TEN_YEARS_IN_SECONDS = 315569520
 
 export const READ_STATUS = [WAMessageStatus.READ, WAMessageStatus.PLAYED]
+
+/**
+ * https://github.com/TextsHQ/baileys/blob/c3084c8ce5f56a0666a143d518381e8b1da747dd/src/Utils/link-preview.ts#L7
+ */
+export const THUMBNAIL_WIDTH_PX = 192
